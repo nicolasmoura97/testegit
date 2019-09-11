10503f142a9b64e1f368440c1183dd430a1f5495
nicolasmoura97 <49923109+nicolasmoura97@users.noreply.github.com>
2019-09-11 17:21:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/',(req,res)=> {
     res.status(200).send({message: 'Olá estou funcionando!'});
 })
+app.get('/Ola',(req,res)=> {
+    res.status(200).send({message: 'Oi eu sou a rota Ola!'});
+})
 app.listen(3001, ()=> {
 
     console.log('Api rodando na porta 3001');
